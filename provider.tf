2565ed30355326a174ea403a0b4af6c0b38fea3d
Amberoat <carolguomin@gmail.com>
2021-10-23 17:31:05 +0800

    add remote

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
 terraform {
+  backend "remote" {
+    organization = "amberone"
+
+    workspaces {
+      name = "didactic-octo-eureka"
+    }
+  }
+
   required_providers {
     azurerm = {
-      source = "Amberoat/didactic-octo-eureka"
       version = "~>2.0"
     }
   }
