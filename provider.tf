29bb4fe14f125d2173672b18f66442d7093b9dd7
Amberoat <carolguomin@gmail.com>
2021-10-23 17:43:04 +0800

    clean up invalid arguments

--- provider.tf
+++ provider.tf
@@ -4,15 +4,10 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    organization = "amberone"
     resource_group_name  = "rg-azure-tf"
     storage_account_name = "saseaazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
-
-    workspaces {
-      name = "didactic-octo-eureka"
-    }
   }
 
   required_providers {
