245158f4c03f5197b73c1dcfaf9b1be22a13f7fc
Amberoat <carolguomin@gmail.com>
2021-10-23 19:49:06 +0800

    remove all unneed vars

--- variables.tf
+++ variables.tf
@@ -1,15 +1,7 @@
-variable "subscription_id" {
-  description = "Azure subscription"
-}
-
 variable "client_id" {
   description = "Azure service principal id"
 }
 
 variable "client_secret" {
   description = "Azure service principal password"
-}
-
-variable "tenant_id" {
-  description = "Azure tenant"
 }
