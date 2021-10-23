2d5578dd1bbf6ecbaf9d60688fcdf7e26ee4adb8
Amberoat <carolguomin@gmail.com>
2021-10-23 19:18:36 +0800

    use terraform cloud

--- provider.tf
+++ provider.tf
@@ -1,17 +1,23 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-azure-tf"
-    storage_account_name = "saseaazuretf"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
+  backend "remote" {
+    # resource_group_name  = "rg-azure-tf"
+    # storage_account_name = "saseaazuretf"
+    # container_name       = "terraform-state"
+    # key                  = "terraform.tfstate"
+    organization = "amberone"
+
+    workspaces {
+      name = "didactic-octo-eureka"
+    }
+
   }
 
   required_providers {
