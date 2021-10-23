8c0cb1b55aa871b3a49537e31cf406930c890dd0
Amberoat <carolguomin@gmail.com>
2021-10-23 22:52:37 +0800

    use d2s v3 for node

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "playground_aks" {
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_D2a_v4"
+    vm_size         = "Standard_D2s_v3"
     os_disk_size_gb = 30
   }
 
