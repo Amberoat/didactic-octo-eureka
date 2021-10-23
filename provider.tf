b41cea86295d7ac1e8d13c6454e861a19f307ea9
Amberoat <carolguomin@gmail.com>
2021-10-23 17:40:18 +0800

    add backend

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 terraform {
   backend "remote" {
     organization = "amberone"
-
-    workspaces {
-      name = "didactic-octo-eureka"
-    }
-
     resource_group_name  = "rg-azure-tf"
     storage_account_name = "saseaazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
+
+    workspaces {
+      name = "didactic-octo-eureka"
+    }
   }
 
   required_providers {
