b6285676d3c9822ce226b731a316fe7e1c22ed93
Amberoat <carolguomin@gmail.com>
2021-10-23 22:41:13 +0800

    change to d2a v4 for aks

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "playground_aks" {
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_B2s"
+    vm_size         = "Standard_D2a_v4"
     os_disk_size_gb = 30
   }
 
